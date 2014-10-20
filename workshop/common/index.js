e7dab97cb70a92c55f31411865f6be2c95c483f3
Hugh Kennedy <hughskennedy@gmail.com>
2014-10-20 04:20:32 +0200

    annotations for javascript too

--- workshop/common/index.js
+++ workshop/common/index.js
@@ -1,11 +1,13 @@
 var highlight  = require('highlight.js').highlight
 var eui        = require('@workshop/exercise-ui')
 var fonts      = require('google-fonts')
+var escape     = require('escape-html')
 var fit        = require('canvas-fit')
 var getCompare = require('gl-compare')
 var getContext = require('gl-context')
 var quotemeta  = require('quotemeta')
 var cheerio    = require('cheerio')
+var glapi      = require('gl-api')
 var marked     = require('marked')
 var glsldoc    = require('glsldoc')
 var assert     = require('assert')
@@ -21,6 +23,14 @@ var exps  = glsldoc.map(function(node) {
 
 var typeExps = new RegExp('([^a-zA-Z_]|^)('+exps+')([^a-zA-Z_]|$)', 'g')
 
+var methods = {}
+var methodExps = glapi.map(function(node) {
+  methods[node.name.slice(3)] = escape(node.description)
+  return quotemeta(node.name.slice(3))
+}).join('|')
+
+methodExps = new RegExp('(gl\.)('+methodExps+')(\\()', 'g')
+
 var markedOpts = {
   highlight: function(code, lang) {
     if (!(lang = lang.trim())) return code
@@ -28,6 +38,9 @@ var markedOpts = {
     code = highlight(lang, code).value
     if (lang === 'glsl') {
       code = annotateGLSL(code)
+    } else
+    if (lang === 'js' || lang === 'javascript') {
+      code = annotateJS(code)
     }
 
     return code
@@ -114,6 +127,30 @@ function annotateGLSL(code) {
     el.html(el.text().replace(typeExps, function(_, pre, type, post) {
       return pre+'<span class="def" title="'+types[type]+'">'+type+'</span>'+post
     }))
+  })
+
+  return code.html()
+}
+
+function annotateJS(code) {
+  code = cheerio.load(code)
+  code.root().contents().map(function(_, el) {
+    el = code(el)
+
+    var html = el.html()
+    var text = el.text()
+    if (html && html !== text) return
+    var updated = false
+
+    text = text.replace(methodExps, function(_, pre, method, post) {
+      updated = true
+      return pre+'<span class="def" title="'+methods[method]+'">'+method+'</span>'+post
+    })
+
+    if (updated) {
+      el.html('<span>'+text+'</span>')
+      el[0].type = 'tag'
+    }
   })
 
   return code.html()
