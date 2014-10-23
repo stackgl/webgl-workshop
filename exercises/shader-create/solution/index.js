7b80f2f4c3c0019f8d6110c15cf779e978ef59c8
Mikola Lysenko <mikolalysenko@gmail.com>
2014-10-22 19:46:01 -0500

    added shader creation exercise

--- exercises/shader-create/solution/index.js
+++ exercises/shader-create/solution/index.js
@@ -1,14 +1,52 @@
-// Magenta is a full mix of red and blue,
-// so we pass them in as 1 (the maxiumum value)
-// while leaving green as 0. "alpha" should be 1 too
-// or the image will be made transparent.
+//This is a helper function to make it easier to get started.
+//You should call this function once your shader is set up
+var drawTriangle = require('./draw-triangle')
 
-var red   = 1
-var green = 0
-var blue  = 1
-var alpha = 1
+//This is the source code for the fragment shader
+var FRAG_SRC = "\
+precision mediump float;\
+void main() {\
+  gl_FragColor = vec4(1, 0, 0, 1);\
+}"
 
+//This is the source code for the vertex shader
+var VERT_SRC = "\
+attribute vec2 position;\
+void main() {\
+  gl_Position = vec4(position,0,1);\
+}"
+
+//TODO: Create this shader in init
+var program
+
+function compileShader(gl, type, src) {
+  var shader = gl.createShader(type)
+  gl.shaderSource(shader, src)
+  gl.compileShader(shader)
+  return shader
+}
+
+// Run once at the beginning: use this to create
+// and setup things to be used in your draw function.
+exports.init = function(gl) {
+
+  var frag = compileShader(gl, gl.FRAGMENT_SHADER, FRAG_SRC)
+  var vert = compileShader(gl, gl.VERTEX_SHADER, VERT_SRC)
+  program = gl.createProgram()
+  gl.attachShader(program, frag)
+  gl.attachShader(program, vert)
+  gl.linkProgram(program)
+}
+
+// Run every frame: use this to draw things to the screen.
 exports.draw = function(gl) {
-  gl.clearColor(red, green, blue, alpha)
+  //Clear drawing buffer
+  gl.clearColor(0,0,0,1)
   gl.clear(gl.COLOR_BUFFER_BIT)
+
+  //Bind shader
+  gl.useProgram(program)
+
+  //Now draw the triangle
+  drawTriangle(gl)
 }
