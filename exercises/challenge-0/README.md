1fff44a11c119db0d401494e82405d039c909c0e
Mikola Lysenko <mikolalysenko@gmail.com>
2014-10-25 16:26:51 -0500

    created a challenge lesson

---
+++ exercises/challenge-0/README.md
@@ -0,0 +1,15 @@
+# First challenge: Draw a triangle
+
+For the first challenge you should combine all the things that you have learned so far about WebGL to draw a single triangle to the screen.
+
+The coordinates for the vertices of the triangle should be:
+
+* `(0,1)`
+* `(-1,0)`
+* `(1,0)`
+
+in normalized screen coordinates.
+
+The triangle should be blue and the background should be yellow.
+
+Good luck!
---
+++ exercises/challenge-0/solution/index.js
@@ -0,0 +1,52 @@
+var FRAG_SRC = "\
+precision mediump float;\
+void main() {\
+  gl_FragColor = vec4(0, 0, 1, 1);\
+}"
+
+//This is the source code for the vertex shader
+var VERT_SRC = "\
+attribute vec2 position;\
+void main() {\
+  gl_Position = vec4(position,0,1);\
+}"
+
+var program, buffer
+
+function compileShader(gl, type, src) {
+  var shader = gl.createShader(type)
+  gl.shaderSource(shader, src)
+  gl.compileShader(shader)
+  return shader
+}
+
+exports.init = function(gl) {
+  var frag = compileShader(gl, gl.FRAGMENT_SHADER, FRAG_SRC)
+  var vert = compileShader(gl, gl.VERTEX_SHADER, VERT_SRC)
+  program = gl.createProgram()
+  gl.attachShader(program, frag)
+  gl.attachShader(program, vert)
+  gl.bindAttribLocation(program, 0, 'position')
+  gl.linkProgram(program)
+
+  buffer = gl.createBuffer(gl.ARRAY_BUFFER)
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+       0, 1,
+      -1, 0,
+       1, 0
+    ]), 
+    gl.STATIC_DRAW)
+
+  gl.enableVertexAttribArray(0)
+  gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0)
+}
+
+exports.draw = function(gl) {
+  gl.clearColor(1,1,0,1)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+
+  gl.useProgram(program)
+
+  gl.drawArrays(gl.TRIANGLES, 0, 3)
+}
