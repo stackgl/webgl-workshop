1f4c3e397c48611be6420fa9cf16d1c610c38408
Hugh Kennedy <hughskennedy@gmail.com>
2014-10-28 10:42:09 +1100

    fix analytics again

--- workshop/common/analytics.js
+++ workshop/common/analytics.js
@@ -8,12 +8,10 @@
 
   /* TODO: also keep count of the number of attempts made per exercise */
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  ;(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  var ga = window.ga
 
   ga('create', 'UA-54792490-2', 'auto')
   ga('send', 'pageview')
