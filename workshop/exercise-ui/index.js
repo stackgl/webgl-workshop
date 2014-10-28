b77f41a4a3c28578a360f8f0cedc21423a3d1433
Hugh Kennedy <hughskennedy@gmail.com>
2014-10-28 12:09:12 +1100

    tooltips for exercise-ui

--- workshop/exercise-ui/index.js
+++ workshop/exercise-ui/index.js
@@ -1,5 +1,6 @@
 var findup    = require('findup-element')
 var fonts     = require('google-fonts')
+var escape    = require('escape-html')
 var css       = require('defaultcss')
 var minstache = require('minstache')
 var inherits  = require('inherits')
@@ -24,6 +25,9 @@ function ExerciseUI() {
   fonts.add({ 'Inconsolata': true })
   css(null, fs.readFileSync(__dirname + '/bundle.css', 'utf8'))
 
+  this.tooltip = document.createElement('div')
+  this.tooltip.classList.add('eui-tooltip')
+
   this.el = document.body.appendChild(
     domify(template(this))
   )
@@ -33,16 +37,31 @@ function ExerciseUI() {
   this.content = this.el.querySelector('.eui-content')
   this.header = this.el.querySelector('.eui-header')
   this.el.addEventListener('click', function(e) {
-    var button = findup(e.target, function(el) {
-      if (!el.parentNode) return
-      if (!el.parentNode.classList) return
-      if (!el.parentNode.classList.contains('eui-icons')) return
-      if (el.nodeName !== 'LI') return
-      return true
-    })
-
+    var button = getButton(e.target)
     if (!button) return
     self.emit(button.title)
+  }, false)
+
+  this.el.addEventListener('mouseover', function(e) {
+    var button = getButton(e.target)
+    if (!button) return
+    document.body.appendChild(self.tooltip)
+    self.tooltip.innerHTML = escape(button.title)
+
+    var bounds = button.getBoundingClientRect()
+    var left   = bounds.left + bounds.width / 2
+    var top    = bounds.top + bounds.height
+
+    self.tooltip.style.left = left + 'px'
+    self.tooltip.style.top  = top + 'px'
+  }, false)
+
+  this.el.addEventListener('mouseout', function(e) {
+    var button = getButton(e.target)
+    if (!button) return
+    if (self.tooltip.parentNode) {
+      document.body.removeChild(self.tooltip)
+    }
   }, false)
 
   this.on('home', function() {
@@ -61,5 +80,15 @@ function ExerciseUI() {
 function get(uri) {
   xhr(uri, function(err, res, body) {
     if (err) throw err
+  })
+}
+
+function getButton(src) {
+  return findup(src, function(el) {
+    if (!el.parentNode) return
+    if (!el.parentNode.classList) return
+    if (!el.parentNode.classList.contains('eui-icons')) return
+    if (el.nodeName !== 'LI') return
+    return true
   })
 }
