26f4d5374f5fef41751a7e97e79028d762ceb2f4
Hugh Kennedy <hughskennedy@gmail.com>
2014-10-28 13:25:41 +1100

    Make node version work as a CLI tool

--- node.js
+++ node.js
@@ -1,4 +1,38 @@
-require('@workshop/server')({}, function(err, address) {
-  if (err) throw err
-  console.log(address)
+#!/usr/bin/env node
+
+var wordwrap = require('wordwrap')
+var inquirer = require('inquirer')
+var opener   = require('opener')
+var chalk    = require('chalk')
+
+console.error()
+console.error(chalk.yellow(' =============================='))
+console.error(chalk.yellow(' = ~~~~~ ')+'webgl-workshop'+chalk.yellow(' ~~~~~ ='))
+console.error(chalk.yellow(' =============================='))
+console.error()
+
+inquirer.prompt([{
+    'type': 'confirm'
+  , 'name': 'ok'
+  , 'default': true
+  , 'message': wordwrap(4, 80)(
+    "We're about to populate this directory with some code for you to " +
+    "use for your answers. If they've already been created then don't worry, " +
+    "they won't be replaced. Continue?"
+  ).replace(/^\s+/, '')
+}], function(result) {
+  if (!result.ok) return process.exit(1)
+
+  console.error()
+  require('@workshop/server')({}, function(err, address) {
+    if (err) throw err
+
+    console.error('Done! Booting up the workshop in your browser in just a second...')
+    console.error('Workshop running on: '+chalk.blue(address))
+    console.error()
+
+    setTimeout(function() {
+      opener(address)
+    }, 1000)
+  })
 })
