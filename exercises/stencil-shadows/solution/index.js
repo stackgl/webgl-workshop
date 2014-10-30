fd1d5bb86d21819ea696e00f27ca5071e89507da
Hugh Kennedy <hughskennedy@gmail.com>
2014-10-30 12:00:23 +1100

    fix viewports in stencil lesson

--- exercises/stencil-shadows/solution/index.js
+++ exercises/stencil-shadows/solution/index.js
@@ -6,6 +6,7 @@ exports.init = function(gl) {
 }
 
 exports.draw = function(gl) {
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
   gl.clearColor(1,1,1,1)
   gl.clearDepth(1)
   gl.clear(
