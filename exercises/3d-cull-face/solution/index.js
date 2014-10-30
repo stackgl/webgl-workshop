0e4fb8a49376e5c7f3aa713eda393a41a3e79e99
Mikola Lysenko <mikolalysenko@gmail.com>
2014-10-31 03:25:41 +1000

    added face culling demo

---
+++ exercises/3d-cull-face/solution/index.js
@@ -0,0 +1,13 @@
+var setup         = require('../submission/setup')
+var drawIt
+
+exports.init = function(gl) {
+  drawIt = setup(gl)
+  gl.cullFace(gl.FRONT)
+  gl.enable(gl.CULL_FACE)
+}
+
+exports.draw = function(gl) {
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
+  drawIt(Date.now())
+}
